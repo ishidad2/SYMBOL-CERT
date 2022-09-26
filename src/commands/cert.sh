4cddf64271042ce40ec900a8352c1f097fed6591
Ubuntu <azureuser@ishida-testnet.ourqon5ajovutojskagsox4gdb.bx.internal.cloudapp.net>
2022-09-26 09:36:42 +0000

    bugfix

--- src/commands/cert.sh
+++ src/commands/cert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo | openssl s_client -showcerts -servername $HOST_IP -connect $HOST_IP:7900 2>/dev/null | openssl x509 -inform pem -noout -text | grep 'Not' > /home/docker/_ssl.log
+echo | openssl s_client -showcerts -servername $HOST -connect $HOST:7900 2>/dev/null | openssl x509 -inform pem -noout -text | grep 'Not' > /home/docker/_ssl.log
